04d15677cbebbdc6207d0f9623645fd45a7d357a
Bhargav261 <55906228+Bhargav261@users.noreply.github.com>
2022-02-15 23:47:28 +0530

    Code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
+import { debounce } from 'lodash';
 
 const Home = () => {
+
+    //Manage Ref and Object
+    const searchRef = useRef();
 
     //Manage State
     const [counter, setCounter] = useState(0);
@@ -36,8 +40,22 @@ const Home = () => {
         setIsLoading(false);
     }
 
-    //Onchnage Event
+    //Debounce Change
+    const debouncedSave = useCallback(
+        debounce(nextValue =>
+            console.log(nextValue)
+            , 500),
+        [],
+    );
 
+    //Usecallback Example
+    const useCallbackEvent = useCallback(
+        () => {
+
+        }, []
+    )
+
+    //Onchnage Event
     const searchChange = (e) => {
         const { name, value } = e.target;
 
@@ -50,6 +68,8 @@ const Home = () => {
             ...res,
             searchText: value
         })
+
+        debouncedSave(value);
     }
 
     //Add Button Click
@@ -65,65 +85,65 @@ const Home = () => {
             ...res,
             searchText: ''
         })
-    }
 
-    console.log("data :- ", data);
+        searchRef.current.focus();
+    }
 
     return (
         <>
             <div className='justify-content-center flex'>
-                <div>
-                    <div className='mt-3 flex'>
-                        {/* <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> */}
-                        <input type="text" class="form-control input-border" name="search" value={res?.searchText} placeholder='Search Item' onChange={searchChange} />
-                        {
-                            res?.searchText != "" && (
-                                <div className='iconDesign cursor-pointer' onClick={clearData}>
-                                    <i className='fa fa-times'></i>
-                                </div>
-                            )
-                        }
+                {isLoading ? (
+                    <div className='justify-content-center flex mt-5'>Please Wait ...</div>
+                ) : (
+                    < div >
+                        <div className='mt-3 p-3 flex pb-0 w-100'>
+                            {/* <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> */}
+                            <input ref={searchRef} type="text" className="form-control input-border" name="search" value={res?.searchText} placeholder='Search Item' onChange={searchChange} />
+                            {
+                                res?.searchText != "" && (
+                                    <div className='iconDesign cursor-pointer' onClick={clearData}>
+                                        <i className='fa fa-times'></i>
+                                    </div>
+                                )
+                            }
 
-                    </div>
-                    <div className='mt-3'>
-                        {
-                            isLoading ? (
-                                <div className='justify-content-center flex'>Please Wait ...</div>
-                            ) : (
-                                <div className='itemBorder' >
-                                    {
-                                        data?.length > 0 ?
+                        </div>
+                        <div className='mt-3 p-3 pt-0'>
+                            <div className='itemBorder scrollWidth' >
+                                {
+                                    data?.length > 0 ?
 
-                                            data?.length > 0 && data.map((item, index) => (
-                                                <>
-                                                    <div key={index} className="borderbtm">
-                                                        <div className="row">
-                                                            <div className="col-md-6">
-                                                                <div className="">
-                                                                    <div>{item.name}</div>
-                                                                    <div className='mt-3'><b>₹{item.price}</b></div>
-                                                                </div>
+                                        data?.length > 0 && data.map((item, index) => (
+                                            <>
+                                                <div key={index} className="borderbtm">
+                                                    <div className="row">
+                                                        <div className="col-md-6">
+                                                            <div className="">
+                                                                <div>{item.name}</div>
+                                                                <div className='mt-3'><b>₹{item.price}</b></div>
                                                             </div>
-                                                            <div className="col-md-6">
+                                                        </div>
+                                                        <div className="col-md-6 justify-content-end flex">
+                                                            <div>
                                                                 <img src={item.cloudinaryImageId} alt={item.name} style={{ width: '150px', height: '150px' }} />
-                                                                <div className='justify-content-center flex mt-2'>
+                                                                <div className='justify-content-center flex mt-3'>
                                                                     <button type="button" className='buttonDesign' name="addButton" onClick={() => buttonClick(item.name)}>
                                                                         Add <i className='fa fa-plus font-13'></i>
-                                                                        </button>
+                                                                    </button>
                                                                 </div>
                                                             </div>
                                                         </div>
-
                                                     </div>
-                                                </>
-                                            ))
-                                            : <div className='mb-3 justify-content-center flex'>Item Not Found !!</div>
-                                    }
-                                </div>
-                            )
-                        }
+
+                                                </div>
+                                            </>
+                                        ))
+                                        : <div className='mb-3 justify-content-center flex'>Item Not Found !!</div>
+                                }
+                            </div>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </>
     )
